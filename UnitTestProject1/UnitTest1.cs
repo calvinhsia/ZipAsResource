a510ccb761d257877b9d9109596504c066f151fe
Calvin Hsia <calvinh@microsoft.com>
2021-07-29 11:46:52 -0700

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZipAsResource;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = Program.GetResource();
+            Assert.AreEqual(276, x.Length);
+        }
+    }
+}
---
+++ ZipAsResource/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipAsResource
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var z =Program.GetResource();
+
+        }
+        public static byte[] GetResource()
+        { 
+            return Properties.Resources.t;
+        }
+    }
+}
